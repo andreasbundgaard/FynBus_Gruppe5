d12df059df8701e70c242da2e14197dac84cae8e
andreasbundgaard <andreasb94@live.dk>
2015-02-02 11:15:32 +0100

    sqlconnection

--- FynBuuus/Controller.cs
+++ FynBuuus/Controller.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace FynBuuus
 {
     class Controller
     {
+        SqlConnection myConnection = new SqlConnection("user id=ejl05_usr;" +
+                                       "password=Baz1nga5;server=ealdb1.eal.local;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=EJL05_DB; " +
+                                       "connection timeout=30");
     }
 }
