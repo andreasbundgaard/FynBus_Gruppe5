bb429e618b7ac1d2bfb1ddf08da77c7adc426bba
Michael Henriksen <micoman13@hotmail.com>
2015-02-02 11:29:21 +0100

    DBConn readded

---
+++ FynBuuus/DB_Connection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FynBuuus {
+    class DB_Connection {
+        public static string Conn = "Server=ealdb1.eal.local;" + "Database=EJL05_DB;" + "User Id=ejl05_usr;" + "Password=Baz1nga5";
+    }
+}
