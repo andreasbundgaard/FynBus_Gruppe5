71e1a9d83be362e6248ca2dee902ae07dfdea952
Michael Henriksen <micoman13@hotmail.com>
2015-02-03 10:09:51 +0100

    Spaces removed

--- FynBuuus/Tilladelser.cs
+++ FynBuuus/Tilladelser.cs
@@ -9,23 +9,14 @@ namespace FynBuuus
     class Tilladelser
     {
         public string TilladelsesNr { get; set; }
-
         public string TilladelseType { get; set; }
-
         public string GyldigTil { get; set; }
-
         public string UdstendeMyndighed { get; set; }
-
         public string RegNummer { get; set; }
-
         public string DatoForKoretojsForsteReg { get; set; }
-
         public string BemaerkningerTilDoku { get; set; }
-
         public string KlarTilDrift { get; set; }
-
         public string TrafikSelskab { get; set; }
-
         public string FT_CVRnr { get; set; }
     }
 }
